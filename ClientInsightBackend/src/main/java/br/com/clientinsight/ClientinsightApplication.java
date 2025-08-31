345a7d0ec098c77ac40ddd00b998bbd48a2160d6
Gabriel Reis <gabrielreistech@gmail.com>
2025-08-31 18:41:32 -0300

    Atualiza .gitignore e remove arquivos ignorados

---
+++ ClientInsightBackend/src/main/java/br/com/clientinsight/ClientinsightApplication.java
@@ -0,0 +1,13 @@
+package br.com.clientinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientinsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientinsightApplication.class, args);
+	}
+
+}
