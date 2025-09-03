1b040d1b9e20e8eaecf53deba3a19dcee36a4051
Gabriel Reis <gabrielreistech@gmail.com>
2025-09-03 19:10:46 -0300

    feat(Frontend): Sistema pronto para começar a criação dos pages e dos components

--- ClientInsightAngular/src/app.module.ts
+++ ClientInsightAngular/src/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { PagesModule } from './pages/pages.module';
+import { ComponentsModule } from './components/components.module';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    PagesModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
